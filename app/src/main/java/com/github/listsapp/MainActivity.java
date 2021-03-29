6a082c1e621883e8d3dc1116a4bb87b34052f140
Maria Frandsen <maria@usloge.dk>
2021-03-29 14:16:42 +0200

    first commit

---
+++ app/src/main/java/com/github/listsapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.github.listsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+    }
+
+
+
+}
